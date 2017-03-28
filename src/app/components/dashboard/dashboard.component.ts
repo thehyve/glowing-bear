477c0965bee80967f4a26e2c0176d571c0b1e8b2
Bo Gao <bo.gao111@gmail.com>
2017-03-28 12:13:50 +0200

    refactor dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'dashboard',
+  selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls:['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css']
 })
+export class DashboardComponent implements OnInit {
 
-export class DashboardComponent {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
