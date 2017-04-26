a82604eefa19b4427573f90f96c0ab0e288ea30a
Bo Gao <bo.gao111@gmail.com>
2017-04-26 11:13:35 +0200

    fix deprecated lamda function in config loading

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,3 +1,4 @@
+
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
@@ -8,6 +9,7 @@ export class AppConfig {
   private config: Object = null;
   private env: Object = null;
 
+  //see this gist: https://gist.github.com/fernandohu/122e88c3bcd210bbe41c608c36306db9
   constructor(private http: Http) {
   }
 
