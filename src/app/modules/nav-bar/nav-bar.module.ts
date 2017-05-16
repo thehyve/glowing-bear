66e1770d04851f92b6aee209acbe2dc7587c1747
Bo Gao <bo.gao111@gmail.com>
2017-05-16 18:41:09 +0200

    optimize nav bar

--- src/app/modules/nav-bar/nav-bar.module.ts
+++ src/app/modules/nav-bar/nav-bar.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavBarComponent} from './nav-bar.component';
 import {RouterModule} from "@angular/router";
+import {TabMenuModule} from "primeng/components/tabmenu/tabmenu";
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    TabMenuModule
   ],
   declarations: [NavBarComponent],
   exports: [NavBarComponent]
