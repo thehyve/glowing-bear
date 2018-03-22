fe669a10ceb07bfd008bd9473552281464c3b209
Bo Gao <bo.gao111@gmail.com>
2018-03-22 12:15:27 +0100

    fix tests

--- src/app/services/transmart-services/transmart-resource.service.spec.ts
+++ src/app/services/transmart-services/transmart-resource.service.spec.ts
@@ -1,11 +1,23 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { TransmartResourceService } from './transmart-resource.service';
+import {TransmartResourceService} from './transmart-resource.service';
+import {HttpClientModule} from '@angular/common/http';
+import {EndpointService} from '../endpoint.service';
+import {EndpointServiceMock} from '../mocks/endpoint.service.mock';
 
 describe('TransmartResourceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TransmartResourceService]
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        TransmartResourceService,
+        {
+          provide: EndpointService,
+          useClass: EndpointServiceMock
+        }
+      ]
     });
   });
 
