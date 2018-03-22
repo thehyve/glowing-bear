4e02c3872b5c694afbec604c57850d5b9b3a0c44
Ewelina Grudzien <ewelina@thehyve.nl>
2018-03-22 14:11:28 +0100

    Fix tests

--- src/app/services/resource-helper.service.spec.ts
+++ src/app/services/resource-helper.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ResourceHelperService } from './resource-helper.service';
+import {ResourceService} from "./resource.service";
+import {ResourceServiceMock} from "./mocks/resource.service.mock";
 
 describe('ResourceHelperService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ResourceHelperService]
-    });
+      providers: [
+        ResourceHelperService,
+          {
+            provide: ResourceService,
+            useClass: ResourceServiceMock
+          }
+        ]
+      });
   });
 
   it('should be created', inject([ResourceHelperService], (service: ResourceHelperService) => {
