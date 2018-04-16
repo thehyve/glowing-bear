3436f4e2fc7f38a349a187d5348bc4aeb139b2f1
barteld klasens <barteld@thehyve.nl>
2018-04-16 12:35:53 +0200

    TMT-142 tree node tests

---
+++ cypress/support/step_definitions/tree-node-steps.js
@@ -0,0 +1,45 @@
+when("I expand the first layer of nodes", () => {
+  cy.get('.ui-tree-toggler').click({ multiple: true });
+});
+
+then("there are {int} sub nodes", (nodeCount) => {
+  cy.get('.ui-treenode-content').should(($p) => {
+    expect($p).to.have.length(nodeCount)
+  })
+});
+
+when("I reveal a numeric node", () => {
+  cy.get('.ui-tree-toggler').eq(0).click();
+});
+
+then("the node has the numeric icon", () => {
+  cy.get('.ui-treenode-icon').eq(1).should('have.class', 'icon-123');
+  cy.get('.ui-treenode-label>span').eq(1).should('contain', 'Heart Rate ');
+});
+
+when("I reveal a categorical node", () => {
+  cy.get('.ui-tree-toggler').eq(4).click()
+});
+
+then("the node has the categorical icon", () => {
+  cy.get('.ui-treenode-icon').eq(5).should('have.class', 'icon-abc');
+  cy.get('.ui-treenode-label>span').eq(5).should('contain', 'Is a Twin ');
+});
+
+when("I reveal a date node", () => {
+  cy.get('.ui-tree-toggler').eq(7).click()
+});
+
+then("the node has the date icon", () => {
+  cy.get('.ui-treenode-icon').eq(8).should('have.class', 'fa-calendar');
+  cy.get('.ui-treenode-label>span').eq(8).should('contain', 'Birth Date ');
+});
+
+when("I reveal a text node", () => {
+  cy.get('.ui-tree-toggler').eq(7).click()
+});
+
+then("the node has the text icon", () => {
+  cy.get('.ui-treenode-icon').eq(11).should('have.class', 'fa-newspaper-o');
+  cy.get('.ui-treenode-label>span').eq(11).should('contain', 'Place of birth ');
+});
