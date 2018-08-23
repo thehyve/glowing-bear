8f542394b30d4c7137d40bf9067efd97ad805a6d
Bo Gao <bo.gao111@gmail.com>
2018-08-23 18:32:45 +0200

    tweak fractalis lib

--- src/app/modules/gb-fractalis-module/gb-fractalis.component.ts
+++ src/app/modules/gb-fractalis-module/gb-fractalis.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication/authentication.service';
+// import * as fjs from '../../../../node_modules/fractalis/lib/fractal-1.2.2-test-1.min.js';
+import * as fjs from 'fractalis';
 
 @Component({
   selector: 'gb-fractalis',
@@ -7,28 +9,29 @@ import {AuthenticationService} from '../../services/authentication/authenticatio
   styleUrls: ['./gb-fractalis.component.css']
 })
 export class GbFractalisComponent implements OnInit {
+  fractal: any;
 
   constructor(private authService: AuthenticationService) {
   }
 
   ngOnInit() {
-    // const config = {
-    //   handler: 'transmart',
-    //   dataSource: 'http://localhost:8081/v2',
-    //   fractalisNode: 'http://localhost:80',
-    //   getAuth() {
-    //     return {token: this.authService.token}
-    //   },
-    //   options: {
-    //     controlPanelPosition: 'right',
-    //     controlPanelExpanded: true
-    //   }
-    // };
-    // console.log('token: ', this.authService.token)
-    // console.log('config: ', config)
-    // let fjs = fractalis.init(config);
-    // // const fjs = init(config);
-    // console.log('fjs', fjs)
+    // TODO: current fractalis testing version does not suport export
+    // TODO: need to add it manually in node_modules
+    // TODO: in future, expect an update from fractalis
+    const config = {
+      handler: 'transmart',
+      dataSource: 'http://localhost:8081/v2',
+      fractalisNode: 'http://localhost:80',
+      getAuth() {
+        return {token: this.authService.token}
+      },
+      options: {
+        controlPanelPosition: 'right',
+        controlPanelExpanded: true
+      }
+    };
+    this.fractal = fjs.fractal.init(config);
+    console.log('fractal', this.fractal)
   }
 
 }
