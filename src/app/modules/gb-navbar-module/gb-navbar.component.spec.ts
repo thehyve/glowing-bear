df23b2d62f09840f34e2757d4e9c80bbe5b44ec9
Bo Gao <bo.gao111@gmail.com>
2018-08-23 21:33:25 +0200

    refactor navbar

--- src/app/modules/gb-navbar-module/gb-navbar.component.spec.ts
+++ src/app/modules/gb-navbar-module/gb-navbar.component.spec.ts
@@ -23,8 +23,8 @@ import {GbMainModule} from '../gb-main-module/gb-main.module';
 describe('GbNavbarComponent', () => {
   let component: GbNavbarComponent;
   let fixture: ComponentFixture<GbNavbarComponent>;
-  let queryService: QueryService;
   let router: Router;
+  let navbarService: NavbarService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,10 +43,6 @@ describe('GbNavbarComponent', () => {
           useValue: '/'
         },
         {
-          provide: QueryService,
-          useClass: QueryServiceMock
-        },
-        {
           provide: NavbarService,
           useClass: NavbarServiceMock
         }
@@ -58,8 +54,8 @@ describe('GbNavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GbNavbarComponent);
     component = fixture.componentInstance;
-    queryService = TestBed.get(QueryService);
     router = TestBed.get(Router);
+    navbarService = TestBed.get(NavbarService);
     router.initialNavigation();
     fixture.detectChanges();
   });
@@ -68,51 +64,16 @@ describe('GbNavbarComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set activeItem', () => {
-    spyOnProperty(component, 'activeItem', 'set').and.callThrough();
-    const dummy = {};
-    component.activeItem = dummy;
-    expect(component.activeItem).toBe(dummy);
-  });
-
-  it('should prevent node drop', () => {
-    let func = function () {
-    };
-    let event = {
-      stopPropagation: func,
-      preventDefault: func
-    };
-    spyOn(component, 'preventNodeDrop').and.callThrough();
-    spyOn(event, 'stopPropagation').and.callThrough();
-    spyOn(event, 'preventDefault').and.callThrough();
-    component.preventNodeDrop(event);
-    expect(component.preventNodeDrop).toHaveBeenCalled();
-    expect(event.stopPropagation).toHaveBeenCalled();
-    expect(event.preventDefault).toHaveBeenCalled();
-  });
-
-  it('should set items', () => {
-    spyOnProperty(component, 'items', 'set').and.callThrough();
-    const dummy = [];
-    component.items = dummy;
-    expect(component.items).toBe(dummy);
-  });
-
-  it('should save query', () => {
-    spyOn(component, 'saveQuery').and.callThrough();
-    component.saveQuery();
-    expect(component.saveQuery).toHaveBeenCalled();
-    // when queryName is defined
-    component.queryName = 'test name';
-    spyOn(queryService, 'saveQueryByName').and.callThrough();
-    component.saveQuery();
-    expect(queryService.saveQueryByName).toHaveBeenCalled();
-  });
-
   it('should handle router events', () => {
     spyOn(router.events, 'subscribe').and.returnValue(event).and.callThrough();
     component.ngOnInit();
     expect(router.events.subscribe).toHaveBeenCalled();
   });
+
+  it('should log out', () => {
+    let spy = spyOn(navbarService, 'logout').and.stub();
+    component.logout();
+    expect(spy).toHaveBeenCalled();
+  })
 
 });
