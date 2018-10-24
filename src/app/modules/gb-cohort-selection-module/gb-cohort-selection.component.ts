593247a07c9212db3a50dd032d88a0ca3fb15cfd
bo <bo.gao111@gmail.com>
2018-10-24 18:08:47 +0200

    merge gb-selection and gb-cohort-selection

--- src/app/modules/gb-cohort-selection-module/gb-cohort-selection.component.ts
+++ src/app/modules/gb-cohort-selection-module/gb-cohort-selection.component.ts
@@ -6,10 +6,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormatHelper} from '../../utilities/format-helper';
 import {QueryService} from '../../services/query.service';
 import {MessageHelper} from '../../utilities/message-helper';
+import {GbConstraintComponent} from "./constraint-components/gb-constraint/gb-constraint.component";
+import {Query} from "../../models/query-models/query";
+import {SubjectSetConstraint} from "../../models/constraint-models/subject-set-constraint";
+import {CombinationConstraint} from "../../models/constraint-models/combination-constraint";
+import {ConstraintService} from "../../services/constraint.service";
+import {TransmartConstraintMapper} from "../../utilities/transmart-utilities/transmart-constraint-mapper";
 
 @Component({
   selector: 'gb-cohort-selection',
@@ -17,10 +23,37 @@ import {MessageHelper} from '../../utilities/message-helper';
   styleUrls: ['./gb-cohort-selection.component.css']
 })
 export class GbCohortSelectionComponent implements OnInit {
+
+  @ViewChild('rootInclusionConstraintComponent') rootInclusionConstraintComponent: GbConstraintComponent;
+  @ViewChild('rootExclusionConstraintComponent') rootExclusionConstraintComponent: GbConstraintComponent;
+
+  private isUploadListenerNotAdded: boolean;
   public queryName: string;
 
-  constructor(public queryService: QueryService) {
+  /**
+   * Split a newline separated string into its parts
+   * and returns a patient set query where these parts are used as subject ids.
+   * @param {string} fileContents the newline separated string.
+   * @param {string} name the query name.
+   * @return {Query} the resulting patient set query.
+   */
+  static processSubjectIdsUpload(fileContents: string, name: string): Query {
+    let subjectIds: string[] = fileContents.split(/[\r\n]+/)
+      .map(id => id.trim())
+      .filter(id => id.length > 0);
+    let query = new Query(null, name);
+    let subjectSetConstraint = new SubjectSetConstraint();
+    subjectSetConstraint.subjectIds = subjectIds;
+    query.subjectQuery = subjectSetConstraint;
+    query.observationQuery = {data: null};
+    return query;
+  }
+
+
+  constructor(public queryService: QueryService,
+              private constraintService: ConstraintService,) {
     this.queryName = '';
+    this.isUploadListenerNotAdded = true;
   }
 
   ngOnInit() {
@@ -40,6 +73,22 @@ export class GbCohortSelectionComponent implements OnInit {
    * @param event
    */
   closeAccordion(event) {
+  }
+
+  get inclusionSubjectCount(): string {
+    return FormatHelper.formatCountNumber(this.queryService.inclusionCounts.subjectCount);
+  }
+
+  get exclusionSubjectCount(): string {
+    return FormatHelper.formatCountNumber(this.queryService.exclusionCounts.subjectCount);
+  }
+
+  get rootInclusionConstraint(): CombinationConstraint {
+    return this.constraintService.rootInclusionConstraint;
+  }
+
+  get rootExclusionConstraint(): CombinationConstraint {
+    return this.constraintService.rootExclusionConstraint;
   }
 
   get subjectCount_0(): string {
@@ -105,6 +154,54 @@ export class GbCohortSelectionComponent implements OnInit {
       this.queryName = '';
     } else {
       MessageHelper.alert('error', 'Please specify the query name.', '');
+    }
+  }
+
+  importCriteria() {
+    let uploadElm = document.getElementById('step1CriteriaFileUpload');
+    if (this.isUploadListenerNotAdded) {
+      uploadElm
+        .addEventListener('change', this.criteriaFileUpload.bind(this), false);
+      this.isUploadListenerNotAdded = false;
+    }
+    // reset the input path so that it will take the same file again
+    uploadElm['value'] = '';
+    uploadElm.click();
+  }
+
+  criteriaFileUpload(event) {
+    let reader = new FileReader();
+    let file: File = event.target.files[0];
+    reader.onload = (function (e: Event) {
+      let data = e.target['result'];
+      let query = this.parseFile(file, data);
+      this.queryService.restoreQuery(query);
+    }).bind(this);
+    reader.readAsText(file);
+  }
+
+  private parseFile(file: File, data: any): Query {
+    if (file.type === 'text/plain' ||
+      file.type === 'text/tab-separated-values' ||
+      file.type === 'text/csv' ||
+      (file.type === '' && file.name.split('.').pop() !== 'json')) {
+      // we assume the text contains a list of subject Ids
+      return GbCohortSelectionComponent.processSubjectIdsUpload(data as string, file.name);
+    } else if (file.type === 'application/json' || file.name.split('.').pop() === 'json') {
+      let _json = JSON.parse(data);
+      // If the json is of standard format
+      if (_json['patientsQuery']) {
+        let name = file.name.substr(0, file.name.indexOf('.'));
+        let query = new Query('', name);
+        query.subjectQuery = TransmartConstraintMapper.generateConstraintFromObject(_json['patientsQuery']);
+        return query;
+      } else {
+        MessageHelper.alert('error', 'Invalid file content for query import.');
+        return;
+      }
+    } else {
+      MessageHelper.alert('error', 'Invalid file format for STEP 1.');
+      return;
     }
   }
 
