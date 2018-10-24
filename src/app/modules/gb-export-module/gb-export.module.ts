3161e82dee6f271e72e680186ce668c693a589e9
bo <bo.gao111@gmail.com>
2018-10-24 15:41:14 +0200

    move data table to export

--- src/app/modules/gb-export-module/gb-export.module.ts
+++ src/app/modules/gb-export-module/gb-export.module.ts
@@ -10,8 +10,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GbExportComponent} from './gb-export.component';
 import {FormsModule} from '@angular/forms';
-import {CheckboxModule, DataTableModule, MessagesModule} from 'primeng/primeng';
+import {CheckboxModule, DataTableModule, MessagesModule, PickListModule, TooltipModule} from 'primeng/primeng';
 import {routing} from './gb-export.routing';
+import {GbDataTableComponent} from "../gb-cohort-selection-module/accordion-components/gb-data-table/gb-data-table.component";
+import {GbDataTableDimensionsComponent} from "./data-table-components/gb-data-table-dimensions/gb-data-table-dimensions.component";
+import {GbDataTableGridComponent} from "./data-table-components/gb-data-table-grid/gb-data-table-grid.component";
+import {TableModule} from "primeng/table";
 
 @NgModule({
   imports: [
@@ -20,9 +24,17 @@ import {routing} from './gb-export.routing';
     FormsModule,
     CheckboxModule,
     DataTableModule,
-    MessagesModule
+    MessagesModule,
+    PickListModule,
+    TableModule,
+    TooltipModule
   ],
-  declarations: [GbExportComponent],
+  declarations: [
+    GbExportComponent,
+    GbDataTableComponent,
+    GbDataTableDimensionsComponent,
+    GbDataTableGridComponent
+  ],
   exports: [GbExportComponent]
 })
 export class GbExportModule {
