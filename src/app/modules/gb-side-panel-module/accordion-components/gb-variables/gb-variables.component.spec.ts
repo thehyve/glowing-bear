038dd861afb16ece70c53506fb0b58157a64cfaf
bo <bo.gao111@gmail.com>
2018-10-26 15:41:53 +0200

    add gb-variables component to side panel

---
+++ src/app/modules/gb-side-panel-module/accordion-components/gb-variables/gb-variables.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GbVariablesComponent } from './gb-variables.component';
+
+describe('GbVariablesComponent', () => {
+  let component: GbVariablesComponent;
+  let fixture: ComponentFixture<GbVariablesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GbVariablesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GbVariablesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/gb-side-panel-module/accordion-components/gb-variables/gb-variables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gb-variables',
+  templateUrl: './gb-variables.component.html',
+  styleUrls: ['./gb-variables.component.css']
+})
+export class GbVariablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
