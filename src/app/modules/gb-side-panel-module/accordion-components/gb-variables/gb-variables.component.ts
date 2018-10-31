3be98238ae53b1afecf371677bd41da58dc595ea
bo <bo.gao111@gmail.com>
2018-10-31 13:54:42 +0100

    add checkboxes to variables when in export

--- src/app/modules/gb-side-panel-module/accordion-components/gb-variables/gb-variables.component.ts
+++ src/app/modules/gb-side-panel-module/accordion-components/gb-variables/gb-variables.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ConstraintService} from '../../../../services/constraint.service';
 import {Concept} from '../../../../models/constraint-models/concept';
 import {ConceptType} from '../../../../models/constraint-models/concept-type';
+import {NavbarService} from '../../../../services/navbar.service';
 
 @Component({
   selector: 'gb-variables',
@@ -9,14 +10,21 @@ import {ConceptType} from '../../../../models/constraint-models/concept-type';
   styleUrls: ['./gb-variables.component.css']
 })
 export class GbVariablesComponent implements OnInit {
+
   private _categorizedVariables: Map<ConceptType, Array<Concept>>;
 
-  constructor(private constraintService: ConstraintService) {
+  public allChecked: boolean;
+  public checkAllText: string;
+
+  constructor(private constraintService: ConstraintService,
+              private navbarService: NavbarService) {
     this.categorizedVariables = new Map<ConceptType, Array<Concept>>();
     this.constraintService.variablesUpdated.asObservable()
       .subscribe((variables: Concept[]) => {
         this.categorizeVariables(variables);
       });
+    this.allChecked = true;
+    this.checkAllText = 'All selected';
   }
 
   ngOnInit() {
@@ -32,6 +40,26 @@ export class GbVariablesComponent implements OnInit {
         this.categorizedVariables.get(variable.type).push(variable);
       }
     });
+<<<<<<< HEAD
+=======
+  }
+
+  onCheckAll(e) {
+    this.checkAllText = e ? 'All selected' : 'None selected';
+    this.checkAll(e);
+  }
+
+  private checkAll(b: boolean) {
+    this.categorizedVariables.forEach((list: Array<Concept>) => {
+      list.forEach((c: Concept) => {
+        c.selected = b;
+      })
+    });
+  }
+
+  get isExport(): boolean {
+    return this.navbarService.isExport;
+>>>>>>> add checkboxes to variables when in export
   }
 
   get availableVariableTypes(): ConceptType[] {
