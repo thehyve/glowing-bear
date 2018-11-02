65724c99c4e47b10d4cf32884a9caebc27029d1e
ewelinagr <ewelina@thehyve.nl>
2018-11-02 15:43:18 +0100

    Remove an infinite loop created during pedigree constraints mapping

--- src/app/utilities/transmart-utilities/transmart-constraint-mapper.ts
+++ src/app/utilities/transmart-utilities/transmart-constraint-mapper.ts
@@ -204,12 +204,11 @@ export class TransmartConstraintMapper {
     constraint = new PedigreeConstraint(constraintObject['relationTypeLabel']);
     (<PedigreeConstraint>constraint).biological = constraintObject['biological'];
     (<PedigreeConstraint>constraint).symmetrical = constraintObject['symmetrical'];
-    let rightHandSide =
-      this.generateConstraintFromObject(constraintObject['relatedSubjectsConstraint']);
+    let rightHandSide = this.generateConstraintFromObject(constraintObject['relatedSubjectsConstraint']);
     (<PedigreeConstraint>constraint).rightHandSideConstraint.children.length = 0;
     if (rightHandSide.className === 'CombinationConstraint') {
-      (<PedigreeConstraint>constraint).rightHandSideConstraint = <CombinationConstraint>rightHandSide;
-      for (let child of (<CombinationConstraint>rightHandSide).children) {
+      let children = (<CombinationConstraint>rightHandSide).children;
+      for (let child of children) {
         (<PedigreeConstraint>constraint).rightHandSideConstraint.addChild(child);
       }
     } else {
