524930e01242c0a132565d2210d3e3c8f631d3e4
bo <bo.gao111@gmail.com>
2018-11-06 12:17:08 +0100

    refactor pedigree constraint

--- src/app/utilities/transmart-utilities/transmart-constraint-mapper.ts
+++ src/app/utilities/transmart-utilities/transmart-constraint-mapper.ts
@@ -203,7 +203,6 @@ export class TransmartConstraintMapper {
     let constraint: Constraint;
     constraint = new PedigreeConstraint(constraintObject['relationTypeLabel']);
     (<PedigreeConstraint>constraint).biological = constraintObject['biological'];
-    (<PedigreeConstraint>constraint).symmetrical = constraintObject['symmetrical'];
     (<PedigreeConstraint>constraint).shareHousehold = constraintObject['shareHousehold'];
     let rightHandSide = this.generateConstraintFromObject(constraintObject['relatedSubjectsConstraint']);
     (<PedigreeConstraint>constraint).rightHandSideConstraint.children.length = 0;
