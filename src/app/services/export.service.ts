117b503a3e8fdabae9c14e40ecbb92cb4f5ae26a
bo <bo.gao111@gmail.com>
2018-11-06 14:58:38 +0100

    fix lint

--- src/app/services/export.service.ts
+++ src/app/services/export.service.ts
@@ -264,8 +264,7 @@ export class ExportService {
         }
       }
     }
-    // 5. Validate if at least one observation is included
-    // TODO: update counts when a subset of variables is selected
+    // TODO: 5. Validate if at least one observation is included
     // let cohortService = this.injector.get(CohortService);
     // if (cohortService.counts_2.observationCount < 1) {
     //   const summary = 'No observation included to be exported.';
