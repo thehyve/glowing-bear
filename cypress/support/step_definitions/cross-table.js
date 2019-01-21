fcaa8467cb305b32ae159e31ec6656c5445e6733
bo <bo.gao111@gmail.com>
2019-01-21 22:37:49 +0100

    add comment regarding drag and drop in cypress for cross table

--- cypress/support/step_definitions/cross-table.js
+++ cypress/support/step_definitions/cross-table.js
@@ -9,6 +9,12 @@ then('A cross table should show up', () => {
   cy.get('.row').last().contains('Row');
 });
 
+/**
+ * Note that for PrimeNg drag n drop, the drop action is only recognizable by Cypress
+ * if the drop zone is a descendant element of its droppable-scoped ancestor,
+ * i.e. the parent has the primeNg directive [pDroppable]=...
+ * also, the descendant element should be a div
+ */
 then('Drag CATEGORICAL_VALUES:Demography:Race from Tree View to row zone', () => {
   cy.toggleNode('Demography');
   cy.drag('Race', '.gb-variables-tree-container').drop(1, 'div.gb-droppable-zone-info-container');
