86ebea0220048f66c3dada1ac51919b7b84ebd6c
Gijs Kant <gijs@thehyve.nl>
2019-09-16 22:56:15 +0200

    Revert change of description of negation on observation constraints.
    After the fix of dimension subselection of 80a5089,
    the description change of 1460956 is not applicable anymore.

--- src/app/modules/gb-cohort-selection-module/constraint-components/gb-constraint/gb-constraint.component.spec.ts
+++ src/app/modules/gb-cohort-selection-module/constraint-components/gb-constraint/gb-constraint.component.spec.ts
@@ -152,7 +152,7 @@ describe('GbConstraintComponent', () => {
     expect(component.observationBoxMessage).toBe('for the patient there is an observation:');
 
     component.constraint.negated = true;
-    expect(component.observationBoxMessage).toBe('for the patient there is an observation other than observations:');
+    expect(component.observationBoxMessage).toBe('for the patient there is NO observation:');
   });
 
 });
