094511e1471cacc512e76266c3b8c9efb5ea3484
Mickaël Misbach <mickael@misba.ch>
2020-01-31 10:45:26 +0100

    fix init error due to libraries expecting node.js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -74,3 +74,9 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+
+(window as any).global = window;
+// @ts-ignore
+window.Buffer = window.Buffer || require('buffer').Buffer;
+import * as process from 'process';
+(window as any).process = process;
