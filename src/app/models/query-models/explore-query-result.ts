dcb8f3675ef111ed790189feb52fd2146295b46d
Mickaël Misbach <mickael@misba.ch>
2020-03-05 13:06:33 +0100

    fix #10: set to zero negative values

--- src/app/models/query-models/explore-query-result.ts
+++ src/app/models/query-models/explore-query-result.ts
@@ -1,5 +1,23 @@
 export class ExploreQueryResult {
-  globalCount: number;
-  perSiteCounts?: number[];
+  private _globalCount: number;
+  private _perSiteCounts?: number[];
   patientLists?: number[][];
+
+  get globalCount(): number {
+    return this._globalCount < 0 ? 0 : this._globalCount;
+  }
+
+  set globalCount(value: number) {
+    this._globalCount = value;
+  }
+
+  get perSiteCounts(): number[] {
+    return this._perSiteCounts.map((perSiteCount: number) =>
+      perSiteCount < 0 ? 0 : perSiteCount
+    );
+  }
+
+  set perSiteCounts(value: number[]) {
+    this._perSiteCounts = value;
+  }
 }
