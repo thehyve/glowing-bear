1d80ffa78a10e722311eefb5804b014ba815de99
Nicolas Freundler <nicolas.freundler@protonmail.ch>
2020-10-15 15:52:29 +0200

    added a confirmation dialog before deleting a cohort

--- src/app/modules/gb-side-panel-module/accordion-components/gb-cohorts/gb-cohorts.component.ts
+++ src/app/modules/gb-side-panel-module/accordion-components/gb-cohorts/gb-cohorts.component.ts
@@ -7,41 +7,47 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import {Component, OnInit, ElementRef, ViewEncapsulation, AfterViewInit} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewEncapsulation, AfterViewInit, ViewChild } from '@angular/core';
 import { Cohort } from 'app/models/cohort-models/cohort';
-import {CohortService, CohortServiceMock } from 'app/services/cohort.service';
+import { CohortService, CohortServiceMock } from 'app/services/cohort.service';
 import { ConstraintService } from 'app/services/constraint.service';
+import { ConfirmationService } from 'primeng/api';
+import { OverlayPanel } from 'primeng/overlaypanel'
 
 
 @Component({
   selector: 'gb-cohorts',
   templateUrl: './gb-cohorts.component.html',
   styleUrls: ['./gb-cohorts.component.css'],
-  encapsulation:ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None,
 })
 export class GbCohortsComponent implements AfterViewInit {
 
   public readonly fileElementId: string = 'cohortFileUpload';
   searchName = '';
-  _changes:MutationObserver;
-  
+  _changes: MutationObserver;
+
+  deletionCandidate: Cohort;
+
   file: File; // holds the uploaded cohort file
 
+  @ViewChild('op', { static: false }) deletionRequest: OverlayPanel;
 
- constructor(public cohortService: CohortServiceMock,
-             private constraintService: ConstraintService,
-             private  element: ElementRef){}
+  constructor(public cohortService: CohortServiceMock,
+    private constraintService: ConstraintService,
+    private confirmationService: ConfirmationService,
+    private element: ElementRef) { }
 
- get cohorts() : Array<Cohort> {
+  get cohorts(): Array<Cohort> {
 
-   return this.cohortService.cohorts
- }
- get selectedCohort() : Cohort{
-   return this.cohortService.selectedCohort
- }
- set selectedCohort(cohort: Cohort){
-   this.cohortService.selectedCohort=cohort
- }
+    return this.cohortService.cohorts
+  }
+  get selectedCohort(): Cohort {
+    return this.cohortService.selectedCohort
+  }
+  set selectedCohort(cohort: Cohort) {
+    this.cohortService.selectedCohort = cohort
+  }
 
   ngAfterViewInit() {
     /*
@@ -54,89 +60,108 @@ export class GbCohortsComponent implements AfterViewInit {
       characterData: true
     })
     */
-    
-    
-  }
-
-  dragdebug(event:DragEvent,cohort:Cohort){
-    event.dataTransfer.setData("text","cohort")
-
-
-    this.cohortService.selectedCohort=cohort
-    
 
 
   }
 
-  refreshCohorts(){
+  dragdebug(event: DragEvent, cohort: Cohort) {
+    event.dataTransfer.setData("text", "cohort")
+
+
+    this.cohortService.selectedCohort = cohort
+
+
+
+  }
+
+  refreshCohorts() {
     this.cohortService.getCohorts()
   }
 
- 
 
 
 
-  toggleBookmark(e: Event, cohort :Cohort){
+
+  toggleBookmark(e: Event, cohort: Cohort) {
     e.stopPropagation()
   }
 
-  downloadCohort(e: Event, cohort: Cohort){
+  downloadCohort(e: Event, cohort: Cohort) {
     e.stopPropagation()
   }
 
-  restoreCohort(e:Event,cohort : Cohort){
+  restoreCohort(e: Event, cohort: Cohort) {
     e.stopPropagation()
-    this.cohortService.selectedCohort=cohort
+    this.cohortService.selectedCohort = cohort
     this.cohortService.restoreTerms()
   }
 
-  bookmarkCohort(e:Event,cohort:Cohort){
+  bookmarkCohort(e: Event, cohort: Cohort) {
     e.stopPropagation()
     cohort.bookmarked = !cohort.bookmarked
   }
 
-  sortByName(){
-    var sorted = this.cohortService.cohorts.sort((a,b)=> (a.name>b.name)?1:-1)
-    this.cohortService.cohorts=sorted
+  sortByName() {
+    var sorted = this.cohortService.cohorts.sort((a, b) => (a.name > b.name) ? 1 : -1)
+    this.cohortService.cohorts = sorted
   }
 
-  sortByBookmark(){
-    var sorted=this.cohortService.cohorts.sort((a,b)=>(!b.bookmarked || a.bookmarked)? -1:1)
-    this.cohortService.cohorts=sorted
+  sortByBookmark() {
+    var sorted = this.cohortService.cohorts.sort((a, b) => (!b.bookmarked || a.bookmarked) ? -1 : 1)
+    this.cohortService.cohorts = sorted
   }
-  sortByDate(){
-    var sorted = this.cohortService.cohorts.sort((a,b) => (!b.creationDate ||
-      a.creationDate && a.creationDate.getTime()>b.creationDate.getTime()
-      ) ? -1:1)
+  sortByDate() {
+    var sorted = this.cohortService.cohorts.sort((a, b) => (!b.creationDate ||
+      a.creationDate && a.creationDate.getTime() > b.creationDate.getTime()
+    ) ? -1 : 1)
 
-      this.cohortService.cohorts=sorted
+    this.cohortService.cohorts = sorted
   }
 
 
-  onFiltering(event :Event){
+  onFiltering(event: Event) {
     var filterWord = this.searchName.trim().toLowerCase()
-    this.cohortService.cohorts.forEach(cohort =>{
-      cohort.visible =(cohort.name.toLowerCase().indexOf(filterWord) === -1) ? false:true
+    this.cohortService.cohorts.forEach(cohort => {
+      cohort.visible = (cohort.name.toLowerCase().indexOf(filterWord) === -1) ? false : true
     })
 
   }
 
-  visibles() : Array<Cohort>{
-    return this.cohorts.filter(cohort=>cohort.visible)
+  visibles(): Array<Cohort> {
+    return this.cohorts.filter(cohort => cohort.visible)
   }
 
-  changeSelect(event :Event,cohort :Cohort){
+  changeSelect(event: Event, cohort: Cohort) {
     event.stopPropagation()
-    this.cohortService.selectedCohort=cohort
+    this.cohortService.selectedCohort = cohort
   }
 
-  remove(event:Event, cohort:Cohort){
+  remove(event: Event, cohort: Cohort) {
     event.stopPropagation()
-    this.cohortService.removeCohorts(cohort)
-    if(this.cohortService.selectedCohort && this.cohortService.selectedCohort ==cohort){
-      this.cohortService.selectedCohort==null
-    }
-    this.cohortService.cohorts=this.cohortService.cohorts.filter(c=>cohort !=c)
+
+    this.deletionCandidate = cohort
+
+
+    this.confirmationService.confirm({
+      message: `Are you sure that you want to permanently remove ${this.deletionCandidate.name} ?`,
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.cohortService.removeCohorts(this.deletionCandidate)
+        if (this.cohortService.selectedCohort && this.cohortService.selectedCohort == cohort) {
+          this.cohortService.selectedCohort == null
+        }
+        this.cohortService.cohorts = this.cohortService.cohorts.filter(c => cohort != c)
+
+      },
+      reject: () => {
+
+      }
+    });
+
+    this.deletionCandidate = undefined
+
+    
   }
 
 
