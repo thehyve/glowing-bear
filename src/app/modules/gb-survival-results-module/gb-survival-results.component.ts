01a3ee99f9865d866b948ef0748a6b66f4437fd8
Nicolas Freundler <nicolas.freundler@protonmail.ch>
2020-11-13 10:28:23 +0100

    added the milestoned summary table

--- src/app/modules/gb-survival-results-module/gb-survival-results.component.ts
+++ src/app/modules/gb-survival-results-module/gb-survival-results.component.ts
@@ -19,7 +19,7 @@ import { logRank2Groups } from 'app/utilities/survival-analysis/log-rank-p-value
 import { ChiSquaredCdf } from 'app/utilities/numerical-methods/chi-squared-cdf';
 import { summaryTable } from 'app/utilities/survival-analysis/summary-table';
 import { PDF } from 'app/models/file-models/pdf';
-import { statTestToTable, summaryToTable } from 'app/utilities/rendering/table-format-for-pdf';
+import { milestonedSummaryToTable, statTestToTable, summaryToTable } from 'app/utilities/rendering/table-format-for-pdf';
 import { ConfidenceInterval } from 'app/models/survival-analysis/confidence-intervals';
 import { ErrorHelper } from 'app/utilities/error-helper';
 
@@ -271,6 +271,11 @@ export class GbSurvivalResultsComponent implements OnInit {
     let curveNames = this.survivalCurve.curves.map(({ groupId }) => groupId)
     pdfDoc.addOneLineText('Summary')
     tables = summaryToTable(curveNames, this.groupTotalAtRisk, this.groupTotalEvent, this.groupTotalCensoring)
+    pdfDoc.addTableFromObjects(tables.headers, tables.data)
+
+    pdfDoc.addOneLineText('Summary at time point')
+    tables = milestonedSummaryToTable(curveNames, this.summaryTableMileStones, this._summaryTable)
+    console.warn('debug tables', tables)
     pdfDoc.addTableFromObjects(tables.headers, tables.data)
 
     if (curveNames.length > 1) {
--- src/app/utilities/rendering/table-format-for-pdf.ts
+++ src/app/utilities/rendering/table-format-for-pdf.ts
@@ -1,3 +1,5 @@
+import { ErrorHelper } from '../error-helper'
+
 export function summaryToTable(
   groupNames: string[],
   groupTotalAtRisk: string[],
@@ -10,6 +12,45 @@ export function summaryToTable(
 
   for (let i = 0; i < nofGroups; i++) {
     data[i] = [groupNames[i], groupTotalAtRisk[i], groupTotalEvent[i], groupTotalCensoring[i]]
+  }
+  return { headers: headers, data: data }
+}
+
+export function milestonedSummaryToTable(
+  groupNames: string[],
+  times: number[],
+  summaries: {
+    atRisk: number;
+    event: number;
+  }[][],
+): { headers: string[][], data: string[][] } {
+  let headers = [['Date time', 'Group name', 'Total at risk', 'Total events']]
+  let nofGroups = groupNames.length
+  let nofTimes = times.length
+  let data = new Array<string[]>()
+
+  if (summaries.length !== nofGroups) {
+    throw ErrorHelper.handleNewError('Unexpected error. Number of group names and number of summaries' +
+      ` are not the same: found ${nofGroups} and ${summaries.length} respectively`)
+  }
+  summaries.forEach(oneGroupSummaries => {
+    if (oneGroupSummaries.length !== nofTimes) {
+      throw ErrorHelper.handleNewError('Unexpected error. Number of times and number of summaries per group' +
+        ` are not the same: found ${nofTimes} and ${oneGroupSummaries.length} respectively`)
+    }
+  })
+
+  for (let i = 0; i < nofTimes; i++) {
+    let datum = new Array<string>(4)
+
+    for (let j = 0; j < nofGroups; j++) {
+      datum[0] = (j === 0) ? `${i}` : ''
+      datum[1] = groupNames[j]
+      datum[2] = summaries[j][i].atRisk.toString()
+      datum[3] = summaries[j][i].event.toString()
+
+    }
+    data.push(datum)
   }
   return { headers: headers, data: data }
 }
