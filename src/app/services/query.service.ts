0228ed4c2cff44da7bef090157027512437f8822
Mickaël Misbach <mickael@misba.ch>
2021-02-05 13:47:57 +0100

    copyright

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -1,6 +1,6 @@
 /**
  * Copyright 2017 - 2018  The Hyve B.V.
- * Copyright 2019 - 2020 LDS EPFL
+ * Copyright 2019 - 2021 LDS EPFL
  * Copyright 2021 CHUV
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
