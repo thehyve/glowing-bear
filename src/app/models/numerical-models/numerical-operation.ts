3a69cd9e19a1dd1286dad3595858cfff91a93c79
Nicolas Freundler <nicolas.freundler@protonmail.ch>
2021-02-17 18:14:03 +0100

    WIP data flow to display numerical method results

---
+++ src/app/models/numerical-models/numerical-operation.ts
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2021 CHUV
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { BehaviorSubject, Observable, of, Subject } from "rxjs"
+import { map } from "rxjs/operators"
+export class NumericalOperation<T, U> {
+  _result: U
+  _errorMessage: string
+  _callback: (survivalAnalysisClear: T) => { res: U, errMessage: string }
+  _data: T
+  _subject: BehaviorSubject<{ res: U, errMessage: string }>
+  _observable: Observable<{ res: U, errMessage: string }>
+
+  private constructor() {
+
+  }
+
+  static NewNumericalOperation<T, U>(survivalAnalysisClear: T, callback: (survivalAnalysisClear: T) => { res: U, errMessage: string }): NumericalOperation<T, U> {
+    let numOperation = new NumericalOperation<T, U>()
+    numOperation._result = null
+    numOperation._errorMessage = null
+    numOperation._callback = callback
+    numOperation._data = survivalAnalysisClear
+    numOperation._subject= new BehaviorSubject({res:numOperation._result,errMessage:numOperation._errorMessage})
+    numOperation._observable = of(numOperation._callback(numOperation._data))
+    numOperation._observable.subscribe(
+      result => {
+        if (result.res) {
+          numOperation._result = result.res
+        }
+        if (result.errMessage) {
+          numOperation._errorMessage = result.errMessage
+        }
+        numOperation._subject.next(result)
+      }
+    )
+    return numOperation
+  }
+
+  addChild<V>(callback: (survivalAnalysisClear: U) => { res: V, errMessage: string }): NumericalOperation<U,V>  {
+    let numOperation = new NumericalOperation<U, V>()
+    numOperation._result = null
+    numOperation._errorMessage = null
+    numOperation._callback = callback
+    numOperation._subject= new BehaviorSubject({res:numOperation._result,errMessage:numOperation._errorMessage})
+    numOperation._observable = this._subject.pipe(map(
+      val => {
+        let returnResult = numOperation._callback(val.res)
+        let errMessage = ((val.errMessage) || val.errMessage === '') ? val.errMessage : returnResult.errMessage
+
+        return { res: returnResult.res, errMessage: errMessage}
+      }
+    ))
+    numOperation._observable.subscribe(
+      result => {
+        if (result.res) {
+          numOperation._result = result.res
+        }
+        if (result.errMessage) {
+          numOperation._errorMessage = result.errMessage
+        }
+        numOperation._subject.next(result)
+      }
+    )
+    return numOperation
+
+  }
+
+  get result(): U {
+    return this._result
+  }
+
+  get errorMessage(): string {
+    return this._errorMessage
+  }
+
+  get finished(): boolean {
+    return ((this.result) || (this.errorMessage)) ? true : false
+  }
+}
+
--- src/app/utilities/survival-analysis/numerical-tables.ts
+++ src/app/utilities/survival-analysis/numerical-tables.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 CHUV
+ * Copyright 2020 - 2021 CHUV
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,6 +9,7 @@ import { logRank2Groups } from './log-rank-p-value'
 import { NewCoxRegression, coxToString } from '../numerical-methods/cox-model'
 import { SurvivalPoint } from 'app/models/survival-analysis/survival-point'
 import { ChiSquaredCdf } from '../numerical-methods/chi-squared-cdf'
+import { NumericalOperation } from 'app/models/numerical-models/numerical-operation'
 
 export function numericalTables(
   curves: SurvivalPoint[][],
@@ -18,26 +19,45 @@ export function numericalTables(
 ): NumericalTablesType {
   const len = curves.length
 
-  let groupLogrankTable = new Array<Array<string>>()
-  let groupCoxRegTable = new Array<Array<string>>()
-  let groupCoxWaldTable = new Array<Array<string>>()
-  let groupCoxLogtestTable = new Array<Array<string>>()
+  let groupLogrankTable = new Array<Array<NumericalOperation<SurvivalPoint[][],string>>>()
+  let groupCoxRegTable = new Array<Array<NumericalOperation<SurvivalPoint[][],string>>>()
+  let groupCoxWaldTable = new Array<Array<NumericalOperation<SurvivalPoint[][],string>>>()
+  let groupCoxLogtestTable = new Array<Array<NumericalOperation<SurvivalPoint[][],string>>>()
 
   let groupTotalEvent = new Array<string>()
   let groupTotalCensoring = new Array<string>()
   let groupTotalAtRisk = new Array<string>()
 
   for (let i = 0; i < len; i++) {
-    let logrankRow = new Array<string>()
-    let coxRegRow = new Array<string>()
-    let waldCoxRow = new Array<string>()
-    let coxLogtestRow = new Array<string>()
+    let logrankRow = new Array<NumericalOperation<SurvivalPoint[][],string>>()
+    let coxRegRow = new Array<NumericalOperation<SurvivalPoint[][],string>>()
+    let waldCoxRow = new Array<NumericalOperation<SurvivalPoint[][],string>>()
+    let coxLogtestRow = new Array<NumericalOperation<SurvivalPoint[][],string>>()
     let totalAtRisk: string
     let totalEvent: string
     let totalCensoring: string
     for (let j = 0; j < len; j++) {
-      let logrank = logRank2Groups(curves[i], curves[j]).toPrecision(stringPrecision)
+      
+      // ------ logrank
+
+      let logrankCallback = (curvesArg:SurvivalPoint[][])=>{
+        let res : string
+        try{
+          res=logRank2Groups(curvesArg[i], curvesArg[j]).toPrecision(stringPrecision)
+        }catch(err){
+          console.warn('y a une erreur')
+          return { res: null, errMessage: 'y a une erreur' }
+        }
+        return { res: res, errMessage: null}
+      }
+      let logrank = NumericalOperation.NewNumericalOperation<SurvivalPoint[][],string>([curves[i], curves[j]],logrankCallback)
+
       logrankRow.push(logrank)
+
+      // ---- cox regression
+      
+
+
       let cox = NewCoxRegression([curves[i], curves[j]], maxIter, tolerance, 'breslow').run()
       let beta = cox.finalBeta[0]
       let variance = cox.finalCovarianceMatrixEstimate[0][0]
